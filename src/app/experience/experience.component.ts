051dcbf4048be9a693c17ea4186683f8d5fc9603
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-03 21:33:20 +0200

    changing Icon for better user experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  selector: "app-experience",
+  templateUrl: "./experience.component.html",
+  styleUrls: ["./experience.component.css"],
 })
 export class ExperienceComponent implements OnInit {
   experiences = [
     {
-      title:"System Developer",
-      company:"Gap Infrastructure Corporation",
-      date:"Feb 2022- Present",
-      duties:[
+      title: "System Developer",
+      company: "Gap Infrastructure Corporation",
+      date: "Feb 2022- Present",
+      duties: [
         "Developing solutions to meet a user’s needs within a system.",
         "Resolving system and user issues.",
         "Develop,testing and validating procedures",
-        ],
-      skills:["Laserfiche","Mtwo"]
-    },{
-      title:"System Support Technician",
-      company:"Gap Infrastructure Corporation",
-      date:"Apr 2021- Mar 2022",
-      duties:[
+      ],
+      skills: ["Laserfiche", "Mtwo"],
+    },
+    {
+      title: "System Support Technician",
+      company: "Gap Infrastructure Corporation",
+      date: "Apr 2021- Mar 2022",
+      duties: [
         "Developing solutions to meet a user’s needs within a system.",
         "Resolving system and user issues.",
         "Develop,testing and validating procedures",
@@ -31,21 +32,11 @@ export class ExperienceComponent implements OnInit {
         "Configuring software platforms to ensure users the best experience",
         "Identifying and assigning employees roles within software platforms",
         "Configuration of software platforms.",
-        ],
-      skills:["Laserfiche","Mtwo"]
-    }
-  ]
-  constructor() { }
+      ],
+      skills: ["Laserfiche", "Mtwo"],
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
-
-
-
-
-
-
-
-
