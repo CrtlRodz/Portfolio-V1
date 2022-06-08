7c0e52d90e6c45f540f08b2578c492e22a386460
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-08 11:26:56 +0200

    Adding project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+  projects =[
+    {
+      ProjectName:"Greg Game",
+      techStack:"Python",
+      library:"Numpy,Pygame,Enum,torch,matplotlib",
+      dateCreated:"12 May 2022",
+      shortDescription:" This Project is based on Reinforcement Learning which trains the snake to eat the food present in the environment.",
+      projectImage:"./assets/img/gregGame.gif"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
