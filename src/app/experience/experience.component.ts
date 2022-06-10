a9faadaae5f5179cbd88c759af25e5170d2c3439
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-10 09:38:24 +0200

    Turning experience into a timeline

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -10,6 +10,8 @@ export class ExperienceComponent implements OnInit {
     {
       title: "System Developer",
       company: "Gap Infrastructure Corporation",
+      companylogo:"./assets/img/GICLogo.png",
+      companyUrl:"https://www.gic.co.za/",
       date: "Feb 2022- Present",
       duties: [
         "Developing solutions to meet a user’s needs within a system.",
@@ -21,6 +23,8 @@ export class ExperienceComponent implements OnInit {
     {
       title: "System Support Technician",
       company: "Gap Infrastructure Corporation",
+      companylogo:"./assets/img/GICLogo.png",
+      companyUrl:"https://www.gic.co.za/",
       date: "Apr 2021- Mar 2022",
       duties: [
         "Developing solutions to meet a user’s needs within a system.",
@@ -29,7 +33,6 @@ export class ExperienceComponent implements OnInit {
         "Store, retrieve, and manipulate data for analysis of system capabilities and requirements",
         "Modify existing software to correct errors",
         "Investigate new solutions to enhance user’s experiences within current software’s.",
-        "Configuring software platforms to ensure users the best experience",
         "Identifying and assigning employees roles within software platforms",
         "Configuration of software platforms.",
       ],
