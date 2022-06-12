c8f48eeee4791b4ea9061541b3b8100efd5ba2c4
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-12 18:01:51 +0200

    finish off experience removing the button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./src/**/*.{html,ts}',"./node_modules/flowbite/**/*.js"],
+    content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
     theme: {
         extend: {},
     },
