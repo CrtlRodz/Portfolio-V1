9de3c00fc14d88542ab2ba98826043b7bb55e684
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-17 22:35:52 +0200

    adding a skill set to about me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,6 +6,39 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./about-me.component.css"],
 })
 export class AboutMeComponent implements OnInit {
+  aboutme = [
+    {
+      name: "Cari-Anne Moore",
+      title: "Software Developer",
+      age: "256",
+    },
+  ];
+  skills = [
+    {
+      name: "Python",
+      logo: "./assets/img/python_icon.png",
+    },
+    {
+      name: "SQL",
+      logo: "./assets/img/sqllogo.png",
+    },
+    {
+      name: "CSS",
+      logo: "./assets/img/csslogo.png",
+    },
+    {
+      name: "Angular",
+      logo: "./assets/img/Angular.png",
+    },
+    {
+      name: "JavaScript",
+      logo: "./assets/img/JavaScrippt.png",
+    },
+    {
+      name: "HTML",
+      logo: "./assets/img/html.png",
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -68,6 +68,7 @@ export class ExperienceComponent implements OnInit {
       skills: ["MyLiberty"],
     },
   ];
+
   constructor() {}
 
   ngOnInit(): void {}
