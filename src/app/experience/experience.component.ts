18104ea422cb3abbcb51c3b53ca1688cecda501d
Cari-Anne Moore <82580186+CrtlRodz@users.noreply.github.com>
2022-06-22 08:59:35 +0200

    Adding Infomration to the about me page

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -40,6 +40,21 @@ export class ExperienceComponent implements OnInit {
     },
     {
       title: "Intern",
+      company: "TonTrac System Analytics",
+      companylogo: "./assets/img/GICLogo.png",
+      companyUrl: "https://salecapital.co/",
+      date: "July 2017 - Oct 2017",
+      duties: [
+        "Installed computer systems and software",
+        "Diagnosed hardware and software issues",
+        "Configuring databases",
+        "Used my interpersonal skills to communicate with employees who faced technical issues",
+        "Managed and maintained records of stocks of equipment.",
+      ],
+      skills: [ "SQL"],
+    },
+    {
+      title: "Intern",
       company: "SalesCapital",
       companylogo: "./assets/img/GICLogo.png",
       companyUrl: "https://salecapital.co/",
@@ -48,7 +63,6 @@ export class ExperienceComponent implements OnInit {
         "Documenting and maintaining the sales capital system.",
         "Adding new Clients to the system.",
         "Configuring databases",
-        "Store, retrieve, and manipulate data for analysis of system capabilities and requirements",
         "Modify existing software to correct errors",
       ],
       skills: ["Bellin", "SQL"],
@@ -60,7 +74,7 @@ export class ExperienceComponent implements OnInit {
       companyUrl: "https://liberty.co.za",
       date: "May 2016 - July 2016",
       duties: [
-        "Working closely with IT Techicans",
+        "Working closely with IT Technicians",
         "Adding new Clients to the system.",
         "Resolving 1st line technical issues.",
         "Working shifts to support internal stakeholders with all IT related queries, system issues, and technical support.",
